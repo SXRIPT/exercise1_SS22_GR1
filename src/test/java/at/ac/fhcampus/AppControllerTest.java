a934395697e13fce8a3ed7d2ce38be79e8d55a61
Georg.fehringer@gmail.com <Tokupe62>
2022-03-03 11:15:05 +0100

    Tests

---
+++ src/test/java/at/ac/fhcampus/AppControllerTest.java
@@ -0,0 +1,28 @@
+package at.ac.fhcampus;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class AppControllerTest {
+
+    @Test
+    void setArticles() {
+    }
+
+    @Test
+    void getArticleCount() {
+    }
+
+    @Test
+    void getTopHeadlinesAustria() {
+    }
+
+    @Test
+    void getAllNewsBitcoin() {
+    }
+
+    @Test
+    void filterList() {
+    }
+}
