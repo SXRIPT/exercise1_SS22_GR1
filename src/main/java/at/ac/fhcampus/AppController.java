810ecb369b08604be8f4cfdad7e7c65300dc622f
Filip Adamovic <45206898+SXRIPT@users.noreply.github.com>
2022-03-03 11:06:48 +0100

    implement diagram

--- src/main/java/at/ac/fhcampus/AppController.java
+++ src/main/java/at/ac/fhcampus/AppController.java
@@ -1,4 +1,33 @@
 package at.ac.fhcampus;
 
+import java.util.List;
+
 public class AppController {
+    private List<Article> articles;
+
+    // Constructor ?
+
+    public void setArticles(List<Article> articles) {
+        this.articles = articles;
+    }
+
+    public int getArticleCount() {
+
+    }
+
+    public List<Article> getTopHeadlinesAustria() {
+
+    }
+
+    public List<Article> getAllNewsBitcoin() {
+
+    }
+
+    protected List<Article> filterList(String query, List<Article> articles) {
+        
+    }
+
+    public List<Article> generateMockList() {
+
+    }
 }
--- src/main/java/at/ac/fhcampus/Menu.java
+++ src/main/java/at/ac/fhcampus/Menu.java
@@ -1,4 +1,39 @@
 package at.ac.fhcampus;
 
 public class Menu {
+    private AppController controller;
+    private String INVALID_INPUT_MESSAGE;
+    private String EXIT_MESSAGE;
+
+    public void start() {
+
+    }
+
+    private void handleInput(String input) {
+
+    }
+
+    private void getArticleCount(AppController ctrl) {
+
+    }
+
+    private void getTopHeadlinesAustria(AppController ctrl) {
+
+    }
+
+    private void getAllNewsBitcoin(AppController ctrl) {
+
+    }
+
+    private void printExitMessage() {
+
+    }
+
+    private void printInvalidInputMessage() {
+
+    }
+
+    private void printMenu() {
+
+    }
 }
