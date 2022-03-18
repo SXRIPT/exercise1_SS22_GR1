66676f9973e06a40c1dacc0a205ae7bce8620350
Georg.fehringer@gmail.com <Tokupe62>
2022-03-18 12:59:09 +0100

    Tests get set Articles

--- src/test/java/at/ac/fhcampus/AppControllerTest.java
+++ src/test/java/at/ac/fhcampus/AppControllerTest.java
@@ -12,6 +12,10 @@ class AppControllerTest {
     @Test
     void setArticles1() {
         AppController test = new AppController();
+        List<Article> inputList = List.of(new Article("Author1", "Title1"), new Article("Author2", "Title2"),
+                new Article("Author3", "Title3"), new Article("Author4", "Title4"));
+        test.setArticles(inputList);
+
         List<Article> testList1 = new ArrayList<Article>();
         testList1.add(new Article("Author1", "Title1"));
         testList1.add(new Article("Author2", "Title2"));
@@ -28,6 +32,9 @@ class AppControllerTest {
     void setArticles2() {
 
         AppController test = new AppController();
+        List<Article> inputList = List.of(new Article("Author1", "Title1"), new Article("Author2", "Title2"),
+                new Article("Author3", "Title3"), new Article("Author4", "Title4"));
+        test.setArticles(inputList);
         List<Article> testList1 = new ArrayList<Article>();
         testList1.add(new Article("Author1", "Title1"));
         testList1.add(new Article("Author2", "Title2"));
