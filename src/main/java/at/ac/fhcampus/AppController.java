00fffe4a50b38c54ee88c323c9c983ad2b2e4d14
Georg.fehringer@gmail.com <Tokupe62>
2022-03-18 11:19:54 +0100

    Tests get set Articles

--- src/main/java/at/ac/fhcampus/AppController.java
+++ src/main/java/at/ac/fhcampus/AppController.java
@@ -4,15 +4,18 @@ import java.util.List;
 
 public class AppController {
     private List<Article> articles;
-
+    private int articleCount;
     // Constructor ?
 
     public void setArticles(List<Article> articles) {
         this.articles = articles;
     }
 
+    public List<Article> getArticles(){
+        return articles;
+    }
     public int getArticleCount() {
-
+        return articleCount;
     }
 
     public List<Article> getTopHeadlinesAustria() {
--- src/test/java/at/ac/fhcampus/AppControllerTest.java
+++ src/test/java/at/ac/fhcampus/AppControllerTest.java
@@ -10,10 +10,30 @@ class AppControllerTest {
 
     @Test
     void setArticles() {
+        AppController test = new AppController();
+        List<Article> testList1 = new ArrayList<Article>();
+        testList1.add(new Article("Author1", "Title1"));
+        testList1.add(new Article("Author2", "Title2"));
+        testList1.add(new Article("Author3", "Title3"));
+        testList1.add(new Article("Author4", "Title4"));
+        List<Article> actual = test.getArticles();
+
+        assertEquals(actual.size(),testList1.size());
+        if(actual.size() == testList1.size()){
+            int sizeList = actual.size();
+            for(int k = 0; k < sizeList; k++){
+                assertEquals(actual.get(k), testList1.get(k));
+            }
+
+        }
+
     }
 
     @Test
     void getArticleCount() {
+        AppController test = new AppController();
+        int articleCount = 7;
+        assertEquals(test.getArticleCount(), articleCount);
     }
 
     @Test
