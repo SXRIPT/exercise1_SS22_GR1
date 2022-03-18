95078b0a126858f6f83475ec76f1666beea25efc
Johannes <johannes.dittmoser-pfeifer@stud.fh-campuswien.ac.at>
2022-03-18 09:08:52 +0100

    Test für getAllBitcoin geschrieben Maven Plugin in pom.xml geholt

--- src/test/java/at/ac/fhcampus/AppControllerTest.java
+++ src/test/java/at/ac/fhcampus/AppControllerTest.java
@@ -2,6 +2,8 @@ package at.ac.fhcampus;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppControllerTest {
@@ -20,6 +22,14 @@ class AppControllerTest {
 
     @Test
     void getAllNewsBitcoin() {
+        AppController test = new AppController();
+        List<Article> actual  = test.getAllNewsBitcoin();
+
+        List<Article> expected = new ArrayList<Article>();
+        expected.add(new Article("Author1","Bitcoin1"));
+        expected.add(new Article("Author2","Bitcoin2"));
+
+        assertEquals(expected.size(),actual.size());
     }
 
     @Test
