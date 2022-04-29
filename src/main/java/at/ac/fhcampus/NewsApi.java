0cfff8dbadfdcb78f51523fb1a93b555289c1af4
Filip Adamovic <45206898+SXRIPT@users.noreply.github.com>
2022-04-29 10:52:22 +0200

    Add request method

--- src/main/java/at/ac/fhcampus/NewsApi.java
+++ src/main/java/at/ac/fhcampus/NewsApi.java
@@ -1,13 +1,45 @@
 package at.ac.fhcampus;
 
-import com.squareup.okhttp.HttpUrl;
+import com.google.gson.Gson;
+import okhttp3.HttpUrl;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Objects;
 
 public class NewsApi {
-    private final String appKey = "aba5826bfd6a41ab980f009db6d19639";
+    private static final String API_KEY = "aba5826bfd6a41ab980f009db6d19639";
+    private static final String BASE_URL = "https://newsapi.org/v2/";
+    private static final String TOP_HEADLINE_ENDPOINT = "top-headlines";
+    private static final String EVERYTHING_ENDPOINT = "everything";
 
+    private final OkHttpClient client = new OkHttpClient();
+    private final Gson gson = new Gson();
+
+    private NewsResponse reqeust(String url) {
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+        try (Response response = client.newCall(request).execute()) {
+            String json = Objects.requireNonNull(response.body()).string();
+            return gson.fromJson(json, NewsResponse.class);
+        } catch (IOException | NullPointerException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private NewsResponse getTopHeadlinesAustria(String query) {
+        return reqeust(BASE_URL + TOP_HEADLINE_ENDPOINT + "apiKey=" + API_KEY + "&q=" + query);
+    }
+
+    private NewsResponse getAllNewsBitcoin(String query) {
+        return reqeust(BASE_URL + EVERYTHING_ENDPOINT + "apiKey=" + API_KEY + "&q=" + query);
+    }
+    /*
     public URL buildUrl(String endpoint,String query, String country){
         URL url = new HttpUrl.Builder()
                 .scheme("https")
@@ -16,8 +48,10 @@ public class NewsApi {
                 .addPathSegment(endpoint)
                 .addQueryParameter("q",query)
                 .addQueryParameter("country",country)
-                .addQueryParameter("apiKey",appKey)
+                .addQueryParameter("apiKey", API_KEY)
                 .build().url();
         return url;
     }
+
+     */
 }
