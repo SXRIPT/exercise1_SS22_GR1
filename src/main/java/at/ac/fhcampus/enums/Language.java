2d1a02641b2b7f17008d76ac4f3f2dfbf820d39a
JuliusBreuss <julius.breuss@stud.fh-campuswien.ac.at>
2022-04-29 11:37:22 +0200

    added enums for language

---
+++ src/main/java/at/ac/fhcampus/enums/Language.java
@@ -0,0 +1,24 @@
+package at.ac.fhcampus.enums;
+
+public enum Language {
+    ARABIC("ar"),
+    GERMAN("de"),
+    ENGLISH("en"),
+    SPANISH("es"),
+    FRENCH("fr"),
+    HEBREW("he"),
+    ITALIAN("it"),
+    DUTCH("nl"),
+    NORWEGIAN("no"),
+    PORTUGESE("pt"),
+    RUSSIAN("ru"),
+    SWEDISH("sv"),
+    URDU("ud"),
+    CHINESE("zh");
+
+    public final String label;
+
+    Language(String label) {
+        this.label = label;
+    }
+}
