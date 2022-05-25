ea9c6bb854cdc94cfd42e52d9c2f5d324db000a6
GeorgFehringer <92082503+GeorgFehringer@users.noreply.github.com>
2022-05-25 10:25:06 +0200

    random stuff +1

--- src/main/java/at/ac/fhcampus/App.java
+++ src/main/java/at/ac/fhcampus/App.java
@@ -1,34 +1,9 @@
 package at.ac.fhcampus;
-
-import org.jetbrains.annotations.NotNull;
-
-import java.util.Collection;
-import java.util.Iterator;
-import java.util.List;
-import java.util.ListIterator;
-import java.util.stream.Stream;
-
 public class App {
     public static void main(String[] args) {
         Menu menu = new Menu();
         menu.start();
     }
 }
-
-
-    // welche source hat meiste artikel
-    List<Article> ArticleListNew;
-    ArticleListNew = new List<Article>;
-Stream<Article> streamFromList = ArticleListNew.stream()
-        .map ()
-
-// welche autor längster name
-        .max (Comparator.comparingInt(Article.getAuthor()::getLength()))
-// wie viele Artikel stammen von source "New York Times"
-streamFromList.
-// welche artikel haben eine Headline die weniger als 15 Zeichen hat
-streamFromList.
-// sortiert die Artikel nach der Länge ihrer Beschreibung aufsteigend; bei gleich alphabetisch
-streamFromList.
 
 
--- src/main/java/at/ac/fhcampus/Menu.java
+++ src/main/java/at/ac/fhcampus/Menu.java
@@ -57,6 +57,24 @@ public class Menu {
 
     }
 
+    /*
+
+    // welche source hat meiste artikel
+    List<Article> ArticleListNew;
+    ArticleListNew = new List<Article>;
+Stream<Article> streamFromList = ArticleListNew.stream()
+        .map ()
+
+// welche autor längster name
+        .max (Comparator.comparingInt(Article.getAuthor()::getLength()))
+// wie viele Artikel stammen von source "New York Times"
+streamFromList.
+// welche artikel haben eine Headline die weniger als 15 Zeichen hat
+streamFromList.
+// sortiert die Artikel nach der Länge ihrer Beschreibung aufsteigend; bei gleich alphabetisch
+streamFromList.
+     */
+
     private void getTopHeadlinesAustria(AppController ctrl) {
         if(ctrl.getTopHeadlinesAustria("Wien") == null) {
             System.out.println(NO_RESULTS_MESSAGE);
